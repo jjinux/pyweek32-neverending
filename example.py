a4890a70cc72068b4c3bd6ed1870aaaf116157a8
JJ Behrens <jj.behrens@udemy.com>
2021-09-21 22:28:32 -0700

    Chaged title and size

--- example.py
+++ example.py
@@ -4,9 +4,9 @@ behrens
 
 import arcade
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SCREEN_TITLE = "My Game"
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
+SCREEN_TITLE = "pyweek32-neverending"
 
 
 class MyGame(arcade.Window):
