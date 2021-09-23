b7090e5537319a2ae66d2b48ed2b87b618a6653f
JJ Behrens <jj.behrens@udemy.com>
2021-09-22 23:10:04 -0700

    Added some comments

--- pw32n/tiles.py
+++ pw32n/tiles.py
@@ -10,5 +10,9 @@ PLAYER_TILE = TileDetails(
     ":resources:images/animated_characters/female_person/femalePerson_idle.png",
     scaling=0.4,
 )
+
+# It's 64x64, and that's what I want.
 GRASS_TILE = TileDetails(":resources:images/topdown_tanks/tileGrass2.png", scaling=1.0)
+
+# It's 128x128, and I want 64x64.
 BOX_CRATE_TILE = TileDetails(":resources:images/tiles/boxCrate_double.png", scaling=0.5)
