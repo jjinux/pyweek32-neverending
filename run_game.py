2f01d4bb2065292465efd9eacd4d411d3c70b668
JJ Behrens <jj.behrens@udemy.com>
2021-09-23 14:53:49 -0700

    Started drawing sprites

--- run_game.py
+++ run_game.py
@@ -8,18 +8,46 @@ MIN_PYTHON_VERSION = (3, 9)
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 650
 SCREEN_TITLE = "pyweek32-neverending"
+CHARACTER_SCALING = 1
+TILE_SCALING = 0.5
 
 
 class MyGame(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        self.wall_list = None
+        self.player_list = None
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
 
     def setup(self):
-        pass
+        self.player_list = arcade.SpriteList()
+        self.wall_list = arcade.SpriteList()
+
+        image_source = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
+        self.player_sprite = arcade.Sprite(image_source)
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 128
+        self.player_list.append(self.player_sprite)
+
+        for x in range(0, 1250, 64):
+            wall = arcade.Sprite(":resources:images/tiles/grassMid.png", TILE_SCALING)
+            wall.center_x = x
+            wall.center_y = 32
+            self.wall_list.append(wall)
+
+        coordinate_list = [[512, 96], [256, 96], [768, 96]]
+
+        for coordinate in coordinate_list:
+            wall = arcade.Sprite(
+                ":resources:images/tiles/boxCrate_double.png", TILE_SCALING
+            )
+            wall.position = coordinate
+            self.wall_list.append(wall)
 
     def on_draw(self):
         arcade.start_render()
+        self.wall_list.draw()
+        self.player_list.draw()
 
 
 def main():
