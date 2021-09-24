fef54920ede203496128afac5e3fb04d3752d2d8
JJ Behrens <jj.behrens@udemy.com>
2021-09-24 11:05:06 -0700

    EnemySprite should have an EnemyModel

--- pw32n/enemy_sprites.py
+++ pw32n/enemy_sprites.py
@@ -1,9 +1,12 @@
 import arcade
 
-from pw32n import sprite_images
+from pw32n import sprite_images, models
 
 
 class EnemySprite(arcade.Sprite):
-    def __init__(self, sprite_image: sprite_images.SpriteImage) -> None:
+    def __init__(
+        self, sprite_image: sprite_images.SpriteImage, model: models.EnemyModel
+    ) -> None:
         super().__init__(sprite_image.filename, sprite_image.scaling)
         self.sprite_image = sprite_image
+        self.model = model
