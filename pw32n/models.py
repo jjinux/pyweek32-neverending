59c1696e8d44fb127358a4070cfef260f8f14a22
JJ Behrens <jj.behrens@udemy.com>
2021-09-24 10:15:04 -0700

    Created a CombatantModel that manages strength and is_dead

--- pw32n/models.py
+++ pw32n/models.py
@@ -1,8 +1,9 @@
-class PlayerModel:
-    MIN_STRENGTH = 1.0
+class Combatant:
+    # Subclasses may want to override this.
+    MIN_STRENGTH = 0.0
 
     def __init__(self) -> None:
-        self.strength = self.MIN_STRENGTH
+        self.__strength = self.MIN_STRENGTH
 
     @property
     def strength(self) -> float:
@@ -11,3 +12,18 @@ class PlayerModel:
     @strength.setter
     def strength(self, strength: float) -> None:
         self.__strength = max(strength, self.MIN_STRENGTH)
+
+    @property
+    def is_dead(self) -> bool:
+        return self.strength == 0.0
+
+
+class PlayerModel(Combatant):
+    # Players can't die.
+    MIN_STRENGTH = 1.0
+
+
+class EnemyModel(Combatant):
+    def __init__(self, strength: float) -> None:
+        super().__init__()
+        self.strength = strength
--- pw32n/models_test.py
+++ pw32n/models_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from pw32n.models import PlayerModel
+from pw32n.models import PlayerModel, EnemyModel
 
 
 class PlayerModelTestCase(unittest.TestCase):
@@ -17,3 +17,14 @@ class PlayerModelTestCase(unittest.TestCase):
     def test_cannot_go_below_min_strength(self) -> None:
         self.player_model.strength = -1.0
         self.assertEqual(self.player_model.strength, self.player_model.MIN_STRENGTH)
+        self.assertFalse(self.player_model.is_dead)
+
+
+class EnemyModelTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.enemy_model = EnemyModel(strength=10.0)
+
+    def test_dying(self) -> None:
+        self.enemy_model.strength -= 100.0
+        self.assertEqual(self.enemy_model.strength, 0.0)
+        self.assertTrue(self.enemy_model.is_dead)
