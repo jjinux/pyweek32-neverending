62903bd4a77f606a8d8d2bceee226a50ae3c1466
JJ Behrens <jj.behrens@udemy.com>
2021-09-23 20:13:29 -0700

    Added tiles for the side view

--- pw32n/tiles.py
+++ pw32n/tiles.py
@@ -7,7 +7,7 @@ class TileDetails(NamedTuple):
 
 
 PLAYER_TILE = TileDetails(
-    ":resources:images/animated_characters/female_person/femalePerson_idle.png",
+    ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png",
     scaling=0.4,
 )
 
@@ -16,3 +16,17 @@ GRASS_TILE = TileDetails(":resources:images/topdown_tanks/tileGrass2.png", scali
 
 # It's 128x128, and I want 64x64.
 BOX_CRATE_TILE = TileDetails(":resources:images/tiles/boxCrate_double.png", scaling=0.5)
+
+# It's 128x128, and I want 64x64.
+GRASS_SIDE_VIEW_TILE = TileDetails(":resources:images/tiles/grassMid.png", scaling=0.5)
+
+# It's 128x128, and I want 128x128.
+PLAYER_SIDE_VIEW_TILE = TileDetails(
+    ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png",
+    scaling=1.0,
+)
+
+# It's 128x128, and I want 128x128.
+SLIME_SIDE_VIEW_TILE = TileDetails(
+    ":resources:images/enemies/slimeGreen.png", scaling=1.0
+)
