46af6fe9d86101772fc2335dfda33c89c249e219
JJ Behrens <jj.behrens@udemy.com>
2021-09-25 07:42:24 -0700

    Added base_strength to BattleMove

--- pw32n/battle_moves.py
+++ pw32n/battle_moves.py
@@ -4,17 +4,27 @@ from pw32n.units import Secs
 
 
 class BattleMove(NamedTuple):
+    base_strength: float
     warmup_period: Secs
     execution_period: Secs
     cooldown_period: Secs
 
 
 DODGE = BattleMove(
-    warmup_period=Secs(0.09), execution_period=Secs(1.0), cooldown_period=Secs(0.3)
+    base_strength=0.0,
+    warmup_period=Secs(0.09),
+    execution_period=Secs(1.0),
+    cooldown_period=Secs(0.3),
 )
 JAB = BattleMove(
-    warmup_period=Secs(0.2), execution_period=Secs(0.3), cooldown_period=Secs(0.2)
+    base_strength=0.1,
+    warmup_period=Secs(0.2),
+    execution_period=Secs(0.3),
+    cooldown_period=Secs(0.2),
 )
 UPPERCUT = BattleMove(
-    warmup_period=Secs(0.8), execution_period=Secs(0.8), cooldown_period=Secs(0.3)
+    base_strength=0.3,
+    warmup_period=Secs(0.8),
+    execution_period=Secs(0.8),
+    cooldown_period=Secs(0.3),
 )
