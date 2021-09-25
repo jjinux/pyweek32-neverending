bf9933b6756ef53959981503a61d2cff9d24d25d
JJ Behrens <jj.behrens@udemy.com>
2021-09-25 11:37:00 -0700

    Added delta_x and delta_y and tweaked the timing

--- pw32n/battle_moves.py
+++ pw32n/battle_moves.py
@@ -9,22 +9,33 @@ class BattleMove(NamedTuple):
     execution_period: Secs
     cooldown_period: Secs
 
+    # Which direction should the sprite move in in order to animate this move
+    # assuming it's the player (-1, 0, or 1)?
+    delta_x: int
+    delta_y: int
+
 
 DODGE = BattleMove(
     base_strength=0.0,
     warmup_period=Secs(0.09),
     execution_period=Secs(1.0),
-    cooldown_period=Secs(0.3),
+    cooldown_period=Secs(0.2),
+    delta_x=-1,
+    delta_y=0,
 )
 JAB = BattleMove(
     base_strength=0.1,
-    warmup_period=Secs(0.2),
-    execution_period=Secs(0.3),
-    cooldown_period=Secs(0.2),
+    warmup_period=Secs(0.15),
+    execution_period=Secs(0.15),
+    cooldown_period=Secs(0.15),
+    delta_x=1,
+    delta_y=0,
 )
 UPPERCUT = BattleMove(
     base_strength=0.3,
-    warmup_period=Secs(0.8),
-    execution_period=Secs(0.8),
-    cooldown_period=Secs(0.3),
+    warmup_period=Secs(0.5),
+    execution_period=Secs(0.5),
+    cooldown_period=Secs(0.2),
+    delta_x=0,
+    delta_y=1,
 )
