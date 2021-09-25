bb8a58dfacc306850e87b4408f723e0edac4042a
JJ Behrens <jj.behrens@udemy.com>
2021-09-24 19:56:31 -0700

    on_update actually uses seconds, not milliseconds

--- pw32n/timed_workflow_test.py
+++ pw32n/timed_workflow_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from pw32n.timed_workflow import MS, TimedWorkflow, TimedStep
+from pw32n.timed_workflow import Secs, TimedWorkflow, TimedStep
 
 
 class TimedWorkflowExample:
@@ -10,11 +10,11 @@ class TimedWorkflowExample:
 
     def __init__(self) -> None:
         self.state = self.STATE_0
-        self.last_late_by: MS = None
+        self.last_late_by: Secs = None
         self.main_workflow = TimedWorkflow(
             [
-                TimedStep(MS(1000.0), self.enter_state_1),
-                TimedStep(MS(1000.0), self.enter_state_2),
+                TimedStep(Secs(1000.0), self.enter_state_1),
+                TimedStep(Secs(1000.0), self.enter_state_2),
             ]
         )
         self.running_workflows: set[TimedWorkflow] = set((self.main_workflow,))
@@ -25,11 +25,11 @@ class TimedWorkflowExample:
         for w in list(self.running_workflows):
             w.on_update(delta_time)
 
-    def enter_state_1(self, late_by: MS) -> None:
+    def enter_state_1(self, late_by: Secs) -> None:
         self.state = self.STATE_1
         self.last_late_by = late_by
 
-    def enter_state_2(self, late_by: MS) -> None:
+    def enter_state_2(self, late_by: Secs) -> None:
         self.state = self.STATE_2
         self.last_late_by = late_by
 
@@ -45,19 +45,19 @@ class TimedWorkflowTestCase(unittest.TestCase):
         self.assertEqual(self.example.state, TimedWorkflowExample.STATE_0)
         self.assertIsNone(self.example.last_late_by)
 
-        self.example.on_update(MS(1.0))
+        self.example.on_update(Secs(1.0))
         self.assertEqual(self.example.state, TimedWorkflowExample.STATE_0)
 
-        self.example.on_update(MS(1000.0))
+        self.example.on_update(Secs(1000.0))
         self.assertEqual(self.example.state, TimedWorkflowExample.STATE_1)
-        self.assertEqual(self.example.last_late_by, MS(1.0))
+        self.assertEqual(self.example.last_late_by, Secs(1.0))
 
-        self.example.on_update(MS(1.0))
+        self.example.on_update(Secs(1.0))
         self.assertEqual(self.example.state, TimedWorkflowExample.STATE_1)
 
-        self.example.on_update(MS(1000.0))
+        self.example.on_update(Secs(1000.0))
         self.assertEqual(self.example.state, TimedWorkflowExample.STATE_2)
-        self.assertEqual(self.example.last_late_by, MS(1.0))
+        self.assertEqual(self.example.last_late_by, Secs(1.0))
 
         self.assertEqual(len(self.example.running_workflows), 0)
 
@@ -65,7 +65,7 @@ class TimedWorkflowTestCase(unittest.TestCase):
         # should get an exception.
         with self.assertRaises(ValueError) as ctx:
             self.example.running_workflows.add(self.example.main_workflow)
-            self.example.on_update(MS(1.0))
+            self.example.on_update(Secs(1.0))
         self.assertEqual(
             ctx.exception.args[0],
             f"You forgot to cleanup your workflow; you should do that in your last step: {self.example.main_workflow}",
